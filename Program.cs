2cb2f40f4db5b479101ab23cfb0c6e6732fcf45c
JErickGM <jerickgm89@gmail.com>
2021-10-16 19:29:39 -0500

    Actualizando repo

--- Program.cs
+++ Program.cs
@@ -1,16 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 using CoreEscuela.Entidades;
+using CoreEscuela.Util;
+using static System.Console;
 
-namespace Etapa1
+namespace CoreEscuela
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria,
-                        ciudad:"Bogotá" , pais:"Colombia"
-                        );
-            Console.WriteLine(escuela);
+            var engine = new EscuelaEngine();
+            engine.Inicializar();
+            Printer.WriteTitle("BIENVENIDOS A LA ESCUELA");
+            //Printer.Beep(10000, cantidad: 10);
+            ImpimirCursosEscuela(engine.Escuela);
+            var listaObjetos = engine.GetObjetosEscuela();
+        }
+
+        private static void ImpimirCursosEscuela(Escuela escuela)
+        {
+
+            Printer.WriteTitle("Cursos de la Escuela");
+
+
+            if (escuela?.Cursos != null)
+            {
+                foreach (var curso in escuela.Cursos)
+                {
+                    WriteLine($"Nombre {curso.Nombre  }, Id  {curso.UniqueId}");
+                }
+            }
         }
     }
 }
